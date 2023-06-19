8acd90438928ca0dbb8a2c5cb348b7a6701f08a4
silvialep <silvia.lepore_sl@libero.it>
2023-06-19 16:47:23 +0200

    feat: handled dynamic search

--- src/pages/AppMain.vue
+++ src/pages/AppMain.vue
@@ -13,16 +13,25 @@ export default{
         developers: [],
         skills: [],
         selectedSkill: '',
+        params: '',
+        isDeveloperFound: false,
       };
   },
   components: { AppDevelopers }, 
 
   methods: {
   getDevelopers(){
-      axios.get(this.URI + this.APIPath + '?skill_id=' + this.selectedSkill ).then(response => {
-      console.log(response.data.results);
-      this.developers = response.data.results;
-      this.skills = response.data.allSkills;
+    axios.get(this.URI + this.APIPath + '?skill_name=' + this.selectedSkill ).then(response => {
+      console.log(response.data);
+      if(response.data.success) {
+        this.developers = response.data.results;
+        this.skills = response.data.allSkills;
+        this.params = response.data.params;
+        this.isDeveloperFound = true;
+      } else {
+        this.isDeveloperFound = false;
+      }
+
     });
   },
 
@@ -40,15 +49,21 @@ export default{
       <input v-model="this.selectedSkill" class="form-control" list="datalistOptions" id="exampleDataList" @change="getDevelopers()" placeholder="Type to search...">
       <datalist id="datalistOptions">
         <option value="">Tutte</option>
-        <option v-for="skill in this.skills " :value="skill.id">{{ skill.name }}</option>
+        <option v-for="skill in this.skills " :value="skill.name">{{ skill.name }}</option>
       </datalist>
       
       <button type="submit">CERCA</button>
 
     </form>
     <h1>Tutti gli sviluppatori</h1>
-    <div class="container all-developers">
+    <h3 v-if="this.params == 'Tutte le specializzazioni'">{{ this.params }}</h3>
+    <h3 v-else>Risultati per la ricerca: {{ this.params }}</h3>
+
+    <div v-if="this.isDeveloperFound" class="container all-developers">
       <AppDevelopers v-for="developer in this.developers" :developers="developer"></AppDevelopers>
+    </div>
+    <div v-else class="alert alert-warning">
+      Non ci sono sviluppatori con questa specializzazione
     </div>
   </div>
 </template>
